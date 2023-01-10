14d862ab1f325ea84ee764bfbf6c59869394ed8d
Daniel Nguyen <daniel.nguye@gmail.com>
2023-01-10 15:28:33 -0500

    generated Comment scaffold, validations, references

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ApplicationRecord
   belongs_to :user
   validates :title, presence: true, length: { maximum: 100 }
   valides :body, presence: true, length: { maximum: 500 }
+  has_many :comments
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
   validates :password, presence: true, length: { in: 6..20 }
   validates :email, presence: true
   has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
 end
