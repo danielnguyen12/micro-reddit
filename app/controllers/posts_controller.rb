a13e6ea50192b7911433b6c4ffaec45b5ba2c34b
Daniel Nguyen <daniel.nguye@gmail.com>
2023-01-10 16:26:21 -0500

    fixed typo regarding Post validation, created default values in seeds.rb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,6 +4,8 @@ class PostsController < ApplicationController
   # GET /posts or /posts.json
   def index
     @posts = Post.all
+    @users = User.all
+    @comments = Comment.all
   end
 
   # GET /posts/1 or /posts/1.json
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+User.create(username:'User1', password: '123456', email: 'hello1@gmail.com')
+User.create(username:'User2', password: 'abcdef', email: 'hello2@gmail.com')
+Post.create(title: 'This is a post by User1', body: 'Information about xyz', user_id: 1)
+Post.create(title: 'This is a post by User2', body: 'Link to xyz', user_id: 2)
+Comment.create(body: 'This is the first comment', user_id: 2, post_id: 1)
